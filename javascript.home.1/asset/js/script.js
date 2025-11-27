3edd2a8f29662b79ee66291ab4b438c4e433cb45
farmanrs <farmanrs@code.edu.az>
2025-11-27 12:17:35 +0400

    feat:fix

---
+++ javascript.home.1/asset/js/script.js
@@ -0,0 +1,90 @@
+// // let name = "Ferman";
+// // let age = 35;
+// // let isStudent = true;
+// // let hobbies = ["futbool", "read book", "codding"];
+// // let address = {
+// //     city: "Baku",
+// //     street: "Nizami küçəsi"
+// // };
+// // console.log(name);
+// // console.log(age);
+// // console.log(isStudent);
+// // console.log(hobbies);
+// // console.log(address);
+
+
+
+// // let ad = "Fərman";              // string
+// // let yas = 22;                   // number
+// // let isStudent = true;           // boolean
+// // let hobbi = ["futbol", "üzmək"]; // array (object tipi)
+// // let unvan = { seher: "Bakı" };  // object
+
+// // console.log(typeof ad);        // string
+// // console.log(typeof yas);       // number
+// // console.log(typeof telebedir); // boolean
+// // console.log(typeof hobbi);     // object
+// // console.log(typeof unvan);     // object
+
+
+// // let celsius = 25;
+// // let fahrenheit = celsius * 1.8 + 32;
+// // console.log("Fahrenheit:", fahrenheit);
+
+
+// // let eded = 17;
+// // if (eded % 2 === 0) {
+// //   console.log(eded + " cütdür");
+// // } else {
+// //   console.log(eded + " təkdir");
+// // }
+
+// // let monthlySalary = 1200;
+// // let yearlySalary = monthlySalary * 12;
+// // console.log("İllik maaş:", yearlySalary);
+
+
+
+
+// // let firstName = "Ferman";
+// // let lastName = "Seferov";
+
+// // console.log(`My full name is ${firstName} ${lastName}.`);
+// // // 8. Dairənin Sahəsinin Hesablanmas
+
+// // let radius = 8;
+// // let area = Math.PI * radius * radius;
+// // console.log("Area:", area);
+
+
+
+// // let text = "JavaScript is awesome!";
+// // console.log(text.length);
+
+// // // Endirimli Qiymətin Tapilmas
+// // let price = 120;
+// // let discount = 20;
+
+// // let finalPrice = price - discount;
+// // console.log("Final price:", finalPrice);
+
+// // let a = 12, b = 45, c = 32;
+
+// // let max = Math.max(a, b, c);
+// // console.log("Max number:", max);
+
+
+
+// let name = "Aylin";
+// let age = 20;
+// let city = baku;
+// console.log(`$(name) is ${age} years old and live in $(city)`);
+
+
+
+
+
+
+
+
+
